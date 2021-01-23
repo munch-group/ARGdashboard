f7b0960e52a2156085b91a718424aae113529e74
Kasper Munch Terkelsen <kmt@ferb.lan.st.client.au.dk>
2021-01-23 09:02:37 +0100

    File restructuring

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 # SEARCH/REPLACE package_name WITH THE ACTUAL PACKAGE NAME BELOW:
 
 setuptools.setup(
-    name='package_name',
+    name='jupyter_popgen_dashboards',
     version="0.1",
     author="Kasper Munch",
     author_email="kaspermunch@birc.au.dk",
     description="Short description",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/kaspermunch/package_name',
+    url='https://github.com/kaspermunch/jupyter_popgen_dashboards',
     packages=setuptools.find_packages(),
     # scripts=['script.py', 'other_script.py'],
     # entry_points = {
