e72a75d53752d545bc4ed8e1e1f0e80a9f94bf13
Kasper Munch <kaspermunch@birc.au.dk>
2023-03-08 15:59:08 +0100

    Bug fix

--- popgen_dashboards/arg_dashboard.py
+++ popgen_dashboards/arg_dashboard.py
@@ -44,7 +44,7 @@ import json
 import pandas as pd
 import networkx as nx
 
-import popgen_dashboards.arg
+import popgen_dashboards.arg as arg
 from popgen_dashboards.arg import Coalescent, Recombination, Leaf, interval_sum, interval_diff, interval_intersect, get_breakpoints, get_child_lineages, rescale_positions, marginal_arg, traverse_marginal, marginal_trees
 
 import plotly.colors
